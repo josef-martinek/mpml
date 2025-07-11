73092a9bcb98e022acf3096051613e49800d01d5
Josef Martinek <jmartinekpe@seznam.cz>
2025-07-11 15:56:57 +0200

    Add lognormal problem for accuracy monitoring

--- setup/mpml_lognormal_minres.py
+++ setup/mpml_lognormal_minres.py
@@ -4,7 +4,7 @@ from dolfinx.fem import form, apply_lifting, Function, functionspace
 from dolfinx.fem.petsc import assemble_matrix, assemble_vector
 import logging
 from setup.lognormal_pde_setup import PDEwLognormalRandomCoeff as PDE
-from core.linsolver.minres import minres
+from core.linsolver.minres import minres, minresMonitor
 
 
 class MPLognormalPDEModelMinres(MPMLModel, LognormalPDEModel):
@@ -36,3 +36,42 @@ class MPLognormalPDEModelMinres(MPMLModel, LognormalPDEModel):
     
     def _get_eval_cost(self, nflops_per_solve):
         return nflops_per_solve
+    
+
+class LognormalMinresMonitor(LognormalPDEModel):
+
+    def evaluate(self):
+        raise NotImplementedError("This class cannot be used in MLMC, only for computing solution stats.")
+
+    def evaluate_monitor(self, level, sample):
+        hl = self.get_hl(level)
+        a, L, bc = PDE.setup_fenics_problem(hl, sample, self._decay_rate_q)
+        a = form(a)
+        L = form(L)
+        A = assemble_matrix(a, bcs=[bc])
+        A.assemble()
+        b = assemble_vector(L)
+        b.assemble()
+        apply_lifting(b.array, [a], bcs=[[bc]])
+        bc.set(b.array)
+
+        # Solve the system and monitor intermediate solutions
+        x, num_it, conv_reason, iterates, rel_residuals = minresMonitor.solve_system(A=A, b=b, rtol=None)
+
+        logging.trace(f'Iteration number on level {level} is {num_it}')
+        logging.trace(f'Convergence reason: {conv_reason}')
+        logging.trace(f'Relative residuals: {rel_residuals}')
+
+        # Compute QoI for all intermediate iterates
+        qois = []
+        msh = PDE.get_mesh(hl)
+        for xi in iterates:
+            uh_i = Function(functionspace(msh, ("Lagrange", 1)))
+            uh_i.x.array[:] = xi.array[:]
+            qoi_i = self._get_qoi_from_solution(uh_i, level)
+            qois.append(qoi_i)
+
+        return {
+            "qois_per_iter": qois,
+            "rel_residuals": rel_residuals,
+        }
